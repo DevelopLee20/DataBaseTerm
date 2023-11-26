49931f1cf7c505fddddc743d6e9499037beae533
LeeInGyu <dldlsrb1414@gmail.com>
2023-11-26 23:26:20 +0900

    어드민 페이지 디자인 완료

--- Student/main.cs
+++ Student/main.cs
@@ -36,45 +36,52 @@ namespace Student
                 String passwd = textBox2.Text;
                 String query;
 
-                if(passwd != "0000")
+                if(val == "관리자" && id == "admin" && passwd == "123123")
                 {
-                    SHA256 hash = new SHA256Managed();
-                    byte[] bytes = hash.ComputeHash(Encoding.ASCII.GetBytes(passwd));
-
-                    StringBuilder sb = new StringBuilder();
-                    foreach (byte b in bytes)
-                    {
-                        sb.AppendFormat("{0:x2}", b);
-                    }
-
-                    passwd = sb.ToString();
-                }
-
-                OleDbCommand cmd = new OleDbCommand();
-                query = $"SELECT * FROM {val} WHERE 학번='{id}' AND 비밀번호='{passwd}'";
-                cmd.CommandText = query;
-                Console.WriteLine($"SELECT * FROM {val} WHERE 학번={id} AND 비밀번호={passwd};");
-                cmd.CommandType = CommandType.Text;
-                cmd.Connection = conn;
-
-                OleDbDataReader read = cmd.ExecuteReader();
-
-                if(!read.Read())
-                {
-                    MessageBox.Show("없는 계정입니다.");
+                    
                 }
                 else
                 {
-                    number = id;
-
-                    if(passwd == "0000")
+                    if (passwd != "0000")
                     {
-                        password_change page = new password_change();
-                        page.Show();
-                    }
-                }
+                        SHA256 hash = new SHA256Managed();
+                        byte[] bytes = hash.ComputeHash(Encoding.ASCII.GetBytes(passwd));
 
-                read.Close();
+                        StringBuilder sb = new StringBuilder();
+                        foreach (byte b in bytes)
+                        {
+                            sb.AppendFormat("{0:x2}", b);
+                        }
+
+                        passwd = sb.ToString();
+                    }
+
+                    OleDbCommand cmd = new OleDbCommand();
+                    query = $"SELECT * FROM {val} WHERE 학번='{id}' AND 비밀번호='{passwd}'";
+                    cmd.CommandText = query;
+                    Console.WriteLine($"SELECT * FROM {val} WHERE 학번={id} AND 비밀번호={passwd};");
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Connection = conn;
+
+                    OleDbDataReader read = cmd.ExecuteReader();
+
+                    if (!read.Read())
+                    {
+                        MessageBox.Show("없는 계정입니다.");
+                    }
+                    else
+                    {
+                        number = id;
+
+                        if (passwd == "0000")
+                        {
+                            password_change page = new password_change();
+                            page.Show();
+                        }
+                    }
+
+                    read.Close();
+                }
             }
             catch (Exception ex)
             {
