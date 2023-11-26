035538ad231a62088e5c6a269f545cd873e06734
LeeInGyu <dldlsrb1414@gmail.com>
2023-11-27 04:48:45 +0900

    성적 등록 및 조회 기능 추가

--- Student/Professor.Designer.cs
+++ Student/Professor.Designer.cs
@@ -37,6 +37,13 @@
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
@@ -126,11 +133,83 @@
             this.label4.TabIndex = 19;
             this.label4.Text = "내 수업 수강 명부";
             // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox3.Location = new System.Drawing.Point(257, 476);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(125, 39);
+            this.textBox3.TabIndex = 20;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox4.Location = new System.Drawing.Point(388, 476);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(125, 39);
+            this.textBox4.TabIndex = 21;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox5.Location = new System.Drawing.Point(519, 476);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(125, 39);
+            this.textBox5.TabIndex = 22;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button2.Location = new System.Drawing.Point(659, 446);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 69);
+            this.button2.TabIndex = 23;
+            this.button2.Text = "성적 등록";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label5.Location = new System.Drawing.Point(282, 446);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 27);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "학번";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label6.Location = new System.Drawing.Point(393, 446);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(120, 27);
+            this.label6.TabIndex = 25;
+            this.label6.Text = "과목번호";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label7.Location = new System.Drawing.Point(546, 446);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 27);
+            this.label7.TabIndex = 26;
+            this.label7.Text = "성적";
+            // 
             // Professor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 800);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.button1);
@@ -160,5 +239,12 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView2;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
--- Student/Professor.cs
+++ Student/Professor.cs
@@ -171,5 +171,78 @@ namespace Student
                 MessageBox.Show("Error: " + ex.Message); //에러 메세지
             }
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(textBox5.Text == "A" || textBox5.Text == "B" || textBox5.Text == "C" || textBox5.Text == "F")
+            {
+                try
+                {
+                    OleDbCommand cmd = new OleDbCommand();
+                    String query = $"UPDATE 수강 SET 성적='{textBox5.Text}' WHERE 학번='{textBox3.Text}' AND 과목번호='{textBox4.Text}'";
+                    cmd.CommandText = query;
+                    cmd.CommandType = CommandType.Text;
+                    String ConnectString = "Provider=MSDAORA;Password=123123;User ID=Term";
+                    OleDbConnection conn = new OleDbConnection(ConnectString);
+                    conn.Open();
+                    cmd.Connection = conn;
+
+                    OleDbDataReader read = cmd.ExecuteReader();
+
+                    MessageBox.Show("성적 등록 완료\n" + query);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message); //에러 메세지
+                }
+
+                try // 내 수업 수강 명부
+                {
+                    dataGridView2.Rows.Clear();
+                    OleDbCommand cmd = new OleDbCommand();
+                    String query = $"SELECT * FROM 수강 WHERE 과목번호 IN (SELECT 과목번호 FROM 개설과목 WHERE 담당교수 = '{main.number}')";
+                    cmd.CommandText = query;
+                    cmd.CommandType = CommandType.Text;
+                    String ConnectString = "Provider=MSDAORA;Password=123123;User ID=Term";
+                    OleDbConnection conn = new OleDbConnection(ConnectString);
+                    conn.Open();
+                    cmd.Connection = conn;
+
+                    OleDbDataReader read = cmd.ExecuteReader();
+
+                    int counts = 5;
+
+                    dataGridView2.ColumnCount = counts;
+
+                    for (int i = 0; i < counts; i++)
+                    {
+                        dataGridView2.Columns[i].Name = read.GetName(i);
+                    }
+
+                    while (read.Read())
+                    {
+                        object[] obj = new object[counts]; // 필드수만큼 오브젝트 배열
+
+                        for (int i = 0; i < counts; i++) // 필드 수만큼 반복
+                        {
+                            obj[i] = new object();
+                            obj[i] = read.GetValue(i); // 오브젝트배열에 데이터 저장
+                        }
+
+                        dataGridView2.Rows.Add(obj); //데이터그리드뷰에 오브젝트 배열 추가
+                    }
+
+                    read.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message); //에러 메세지
+                }
+            }
+            else
+            {
+                MessageBox.Show("성적은 A, B, C, F 중 하나만 입력 가능합니다.");
+            }
+        }
     }
 }
